2b9c42f9435f73924c47f1df8696c37c025cef9c
Fortune Ekeruo <legalmody@gmail.com>
2017-04-06 12:34:24 +0100

    Use style and css loaders to read styles into the page

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import greetUser from './first-module';
 import  playMusic from './second-module';
+import './styles/main.css';
 
 greetUser('Iyke', document.querySelector('h1'));
 playMusic('Usher', document.querySelector('p'));
